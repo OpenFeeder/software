b35c44d296610a6f5c56ce28a6b7340622dabd99
JeromeBriot <jbtechlab@gmail.com>
2017-12-07 16:34:50 +0100

    Add try-except statement in case serial port not found

--- serial/python/setdatetime/of_setdatetime.py
+++ serial/python/setdatetime/of_setdatetime.py
@@ -8,57 +8,59 @@ Created on Wed Dec  6 09:50:33 2017
 import serial
 import time
 
-delay = 0.03
+delay = 0.05
 
 serialPort = "COM36"
 
-ser = serial.Serial(port=serialPort, baudrate=9600, timeout=1, writeTimeout=1)
+try:
+    
+    ser = serial.Serial(port=serialPort, baudrate=9600, timeout=1, writeTimeout=1)
 
-if not ser.isOpen():
-
-    ser.open()
-
-ligne = ser.read_all()
-
-ser.write(b't')
-
-time.sleep(delay*5)
-
-ligne = ser.read_all()
-
-print('{}'.format(ligne.decode("utf-8")))
-
-ser.write(b's')
-
-time.sleep(delay*5)
-ligne = ser.read_all()
-
-current_time = time.localtime()
-
-b = bytes(str(current_time.tm_mday), 'ascii') + b'\r'
-ser.write(b)
-b = bytes(str(current_time.tm_mon), 'ascii') + b'\r'
-ser.write(b)
-b = bytes(str(current_time.tm_year-2000), 'ascii') + b'\r'
-ser.write(b)
-
-time.sleep(delay*5)
-ligne = ser.read_all()
-
-b = bytes(str(current_time.tm_hour), 'ascii') + b'\r'
-ser.write(b)
-b = bytes(str(current_time.tm_min), 'ascii') + b'\r'
-ser.write(b)
-b = bytes(str(current_time.tm_sec), 'ascii') + b'\r'
-ser.write(b)
-
-time.sleep(delay*5)
-ligne = ser.read_all()
-
-ser.write(b't')
-time.sleep(delay*5)
-ligne = ser.read_all()
-
-print('{}'.format(ligne.decode("utf-8")))
-
-ser.close()
+    ligne = ser.read_all()
+    
+    ser.write(b't')
+    
+    time.sleep(delay*5)
+    
+    ligne = ser.read_all()
+    
+    print('{}'.format(ligne.decode("utf-8")))
+    
+    ser.write(b's')
+    
+    time.sleep(delay*5)
+    ligne = ser.read_all()
+    
+    current_time = time.localtime()
+    
+    b = bytes(str(current_time.tm_mday), 'ascii') + b'\r'
+    ser.write(b)
+    b = bytes(str(current_time.tm_mon), 'ascii') + b'\r'
+    ser.write(b)
+    b = bytes(str(current_time.tm_year-2000), 'ascii') + b'\r'
+    ser.write(b)
+    
+    time.sleep(delay*5)
+    ligne = ser.read_all()
+    
+    b = bytes(str(current_time.tm_hour), 'ascii') + b'\r'
+    ser.write(b)
+    b = bytes(str(current_time.tm_min), 'ascii') + b'\r'
+    ser.write(b)
+    b = bytes(str(current_time.tm_sec), 'ascii') + b'\r'
+    ser.write(b)
+    
+    time.sleep(delay*5)
+    ligne = ser.read_all()
+    
+    ser.write(b't')
+    time.sleep(delay*5)
+    ligne = ser.read_all()
+    
+    print('{}'.format(ligne.decode("utf-8")))
+    
+    ser.close()
+    
+except:
+    
+    print('\nPB: check serial port number on line 13 in the Python script')
